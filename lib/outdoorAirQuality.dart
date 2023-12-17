fb3412c9549e558400e83bfd915be98d2524862f
ebtisam07 <439200954@student.ksu.edu.sa>
2023-12-17 19:41:20 +0300

    ..

--- lib/outdoorAirQuality.dart
+++ lib/outdoorAirQuality.dart
@@ -30,7 +30,7 @@ class OutdoorAirQuality {
     }
     time = DateTime.parse((data as Map)['received_at']);
     dust = Sensor.dust;
-    List<dynamic> readings = [temp, hum, dust, time];
+    List<dynamic> readings = [temp, hum, 400, time];
     return readings;
   }
 
@@ -99,37 +99,12 @@ class OutdoorAirQuality {
   Widget viewOutdoorAirQuality(List<dynamic> readings, context) {
     List<Map<String, dynamic>> levels = calculateLevel(readings);
     Map<String, Color> airQuality_color = calculateAirQuality(levels);
-    String first = FirebaseService.first_name;
+
     String levelImg = footerImg(levels);
     String leveltxt = footerTxt(levels);
     String airQuality = airQuality_color.keys.first;
     Color color = airQuality_color.values.first;
-    TextSpan nameSpan = TextSpan(
-      text: "مرحباً " + first + "\n",
-      style: GoogleFonts.robotoCondensed(
-        fontWeight: FontWeight.bold,
-        color: Color(0xFF019CB2),
-        fontSize: 20,
-      ),
-    );
 
-    TextSpan restOfTextSpan = TextSpan(
-      text: leveltxt,
-      style: GoogleFonts.robotoCondensed(
-        fontWeight: FontWeight.bold,
-        fontSize: 17,
-      ),
-    );
-
-    // Combine the TextSpans into a RichText widget
-    Text richText = Text.rich(
-      TextSpan(
-        children: [
-          nameSpan,
-          restOfTextSpan,
-        ],
-      ),
-    );
     return Expanded(
       child: Column(
         children: [
@@ -217,41 +192,23 @@ class OutdoorAirQuality {
               ],
             ),
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 250, // Set the desired width
-                    child: richText,
-                  ),
-                  /*Text(
-                    "مرحباً" + first + "\n",
-                    style: GoogleFonts.robotoCondensed(
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF019CB2),
-                      fontSize: 20,
-                    ),
-                  ),
-                  Text(
-                    leveltxt,
-                    style: GoogleFonts.robotoCondensed(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
-                    ),
-                  ),*/
-                  Image.asset(
-                    levelImg,
-                    width: 300,
-                  ),
-                ],
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            // First item
+            child: Text(
+              leveltxt,
+              style: GoogleFonts.robotoCondensed(
+                fontWeight: FontWeight.bold,
+                fontSize: 17,
               ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            // Second item
+            child: Image.asset(
+              levelImg,
+              width: 270,
             ),
           ),
         ],
